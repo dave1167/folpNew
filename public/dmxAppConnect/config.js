4c97e7058739c6c03540d7a9afbda666b2e64eda
Dave Rowe <daverowe1167@gmail.com>
2025-12-15 07:33:29 +0000

    sort not working only one way

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -10,5 +10,29 @@ dmx.config({
         "name": "sort"
       }
     ]
+  },
+  "members_list": {
+    "query": [
+      {
+        "type": "text",
+        "name": "dir"
+      },
+      {
+        "type": "text",
+        "name": "sort"
+      }
+    ]
+  },
+  "list_members": {
+    "query": [
+      {
+        "type": "text",
+        "name": "dir"
+      },
+      {
+        "type": "text",
+        "name": "sort"
+      }
+    ]
   }
 });
